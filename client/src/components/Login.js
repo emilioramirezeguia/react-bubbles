88bbcaa953b3145473ced20fcea6e6173b29b912
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-07-24 10:33:58 -0500

    Update loginUser function to retrieve token from API and save it to localStorage

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -20,12 +20,14 @@ const Login = () => {
     axios
       .post("http://localhost:5000/api/login", user)
       .then((response) => {
-        console.log(response);
+        localStorage.setItem("token", response.data.payload);
       })
       .catch((error) => {
         console.log(error);
       });
+    setUser(initialUser);
   };
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
